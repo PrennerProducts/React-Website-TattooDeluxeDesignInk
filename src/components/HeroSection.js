41d24a5a245a4733396d841f521864b1deed4c10
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-01-13 00:24:11 +0100

    Video funkt nicht, Navbar finito

---
+++ src/components/HeroSection.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../App.css'
+import { Button } from './Button'
+import './HeroSection.css'
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+        
+        <video src="/public/videos/video-2.mp4" />autoPlay loop muted />
+        <h1>WATCH NOW</h1>
+        <p>What are you waiting for?</p>
+        <div className="btns">
+            <Button 
+            className="btns" 
+            buttonStyle="btn--outline" 
+            buttonSize="btn--large"
+            >
+                GET STARTED
+            </Button>
+            <Button 
+            className="btns" 
+            buttonStyle="btn--primary" 
+            buttonSize="btn--large"
+            >
+                WATCH TRAILER <i className="far fa-play-circle" />
+            </Button>
+
+    </div>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 //import { skull } from '.../img/skull-icon.png';
+import './Navbar.css';
+
+
+
 
 function MyNavbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenue = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TATTOO <i className="fas fa-sharp fa-solid fa-skull" />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenue}>
+            PP Ink TattooStudio <i className="fas fa-sharp fa-solid fa-skull" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to={"/"} className="nav-links" onClick={closeMobileMenue}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/uebermich" className="nav-links" onClick={closeMobileMenue}>
+                Über mich
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/portfolio" className="nav-links" onClick={closeMobileMenue}>
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/kontakt" className="nav-links" onClick={closeMobileMenue}>
+                Kontakt
+              </Link>
+            </li>    
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>

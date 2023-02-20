b26c5a98239abcb235c23919bd069a1a346af63a
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-02-20 22:53:08 +0100

    router update to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
-// import './App.css';
-// import React from 'react';
-// import MyNavbar from './components/Navbar';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Home from './components/pages/Home';
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import UeberMich from './components/pages/UeberMich';
+import MyNavbar from './components/Navbar';
+import Home from "./components/pages/Home";
+import Gallerie from "./components/pages/Gallerie.js";
+import Contact from './components/pages/Contact.js';
+import Footer from './components/pages/Footer.js';
+import ErrorPage from './error-page.js';
+import HeroSection from './components/HeroSection';
+import { Navbar } from 'react-bootstrap';
+import Impressum from './components/pages/impressum.js';
+import './index.css';
 
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <MyNavbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
 
-// export default App;
+export default function App() {
+  return (
+    <div>
+       
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="uebermich" element={<UeberMich />} />
+            <Route path="gallerie" element={<Gallerie />} />
+          <Route path="contact" element={<Contact />} />
+            <Route path="impressum" element={<Impressum />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <MyNavbar />
+      
+     
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+      
+    </div>
+  );
+}
+
+
+
+
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,106 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-import MyNavbar from './components/Navbar';
-import Home from "./components/pages/Home";
-import Gallerie from "./components/pages/Gallerie.js";
-import UeberMich from './components/pages/UeberMich.js';
-import Contact from './components/pages/Contact.js';
-import Footer from './components/pages/Footer.js';
-import ErrorPage from './error-page.js';
-import HeroSection from './components/HeroSection';
-import { Navbar } from 'react-bootstrap';
-import Impressum from './components/pages/impressum.js';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
 
-const AppLayout = () => {
-    return (
-        <>
-        
-        <MyNavbar />
-        
-        <div className='layout-container'>
-       
-            <div className='layout-item'>
-            <UeberMich/>
-            </div>
-      
-            <div className='layout-item'>
-            <Contact/>
-            </div>
-
-            <div className='layout-item'>
-            <Gallerie/>
-            </div>
-            
-          
-         
-        </div>
-       
-        
-
-        <Outlet/>
-       
-       
-      
-        
-        </>
-    );
-};
-
-const router = createBrowserRouter([
-
-    {
-        element: <AppLayout/>,
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            path: "/",
-            element: <Home/>,
-            errorElement: <ErrorPage />,
-           },
-
-          {
-          path: "/uebermich",
-          element: <UeberMich/>,
-          errorElement: <ErrorPage />,
-          },
-
-          {
-          path: "/contact",
-          element: <Contact/>,
-          errorElement: <ErrorPage />,
-          },
-
-          {
-            path: "/gallerie",
-            element: <Gallerie/>,
-            errorElement: <ErrorPage />,
-          },
-        
-        
-         
-
-      
-        ],
-    },
-    {
-      path: "/impressum",
-      element: <Impressum/>,
-     },
-   
-  
-]);
+import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-   
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-export default AppLayout;

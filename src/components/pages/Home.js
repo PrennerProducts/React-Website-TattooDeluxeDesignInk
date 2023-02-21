415491812d76f2fbd003890c51db2f68db67f20d
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-02-21 14:21:39 +0100

    update gpt

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../App.css';
 import HeroSection from '../HeroSection';
+import Contact from './Contact';
 import Gallerie from './Gallerie';
 import UeberMich from './UeberMich';
 
@@ -13,12 +14,16 @@ function Home() {
     };
     return (
         componentDidMount(),
-        <>
+       
+        <div className="home-container">
         <HeroSection />
-        
+        <div className="home-grid">
+        <UeberMich className="uebermich" />
+        <Contact className="contact" />
+        <Gallerie className="gallerie" />
+      </div>
+    </div>
+  );
+}
 
-        </>
-    );
-    }
-
-    export default Home;
+export default Home;

8f4de26884ab5fe413b4468cac60de696431b127
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-02-21 15:24:37 +0100

    Revert "router update to app.js"re
    This reverts commit b26c5a98239abcb235c23919bd069a1a346af63a.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import MyNavbar from './components/Navbar';
+import Home from "./components/pages/Home";
+import Gallerie from "./components/pages/Gallerie.js";
+import UeberMich from './components/pages/UeberMich.js';
+import Contact from './components/pages/Contact.js';
+import Footer from './components/pages/Footer.js';
+import ErrorPage from './error-page.js';
+import HeroSection from './components/HeroSection';
+import { Navbar } from 'react-bootstrap';
+import Impressum from './components/pages/impressum.js';
+import './index.css';
 
-import "./index.css";
-import App from "./App";
+const AppLayout = () => {
+    return (
+        <>
+        
+        <MyNavbar />
+        
+        <div className='layout-container'>
+       
+            <div className='layout-item'>
+            <UeberMich/>
+            </div>
+      
+            <div className='layout-item'>
+            <Contact/>
+            </div>
+
+            <div className='layout-item'>
+            <Gallerie/>
+            </div>
+            
+          
+         
+        </div>
+       
+        
+
+        <Outlet/>
+       
+       
+      
+        
+        </>
+    );
+};
+
+const router = createBrowserRouter([
+
+    {
+        element: <AppLayout/>,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            path: "/",
+            element: <Home/>,
+            errorElement: <ErrorPage />,
+           },
+
+          {
+          path: "/uebermich",
+          element: <UeberMich/>,
+          errorElement: <ErrorPage />,
+          },
+
+          {
+          path: "/contact",
+          element: <Contact/>,
+          errorElement: <ErrorPage />,
+          },
+
+          {
+            path: "/gallerie",
+            element: <Gallerie/>,
+            errorElement: <ErrorPage />,
+          },
+        
+        
+         
+
+      
+        ],
+    },
+    {
+      path: "/impressum",
+      element: <Impressum/>,
+     },
+   
+  
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+   
   </React.StrictMode>
 );
+
+export default AppLayout;

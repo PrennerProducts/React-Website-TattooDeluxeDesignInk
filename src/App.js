1736f7e43787955a49db48ce36d4c67c8826ca82
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-02-22 15:44:53 +0100

    update und finish, +emailjs

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default function App() {
              />
        </div>
       :
-        <div>
+        <div className='app-route'>
     
        
       {/* Routes nest inside one another. Nested route paths build upon

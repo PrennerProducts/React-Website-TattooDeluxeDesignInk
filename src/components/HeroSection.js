551d12e9a5168299266d829c580627ebab4505fc
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-02-28 23:48:54 +0100

    update deploy wit face video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -51,7 +51,7 @@ function HeroSection() {
     />
        </div>
        <img className='herologo' src="/images/white_logo_transparent_background.png" alt="logocube"  />    
-       <video  src="/videos/Tattoo_zoom.mp4" autoPlay loop muted  />
+       <video  src="/videos/tattoo-44579.mp4" autoPlay loop muted   alt="/images/contact2.jpg"/>
       
       
       
